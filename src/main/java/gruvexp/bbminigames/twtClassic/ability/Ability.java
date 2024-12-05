92f521172f48b6953b26d8266ec508e284f166ae
GruveXp <gruveexp123@gmail.com>
2024-12-05 22:09:10 +0100

    add: new ability class

---
+++ src/main/java/gruvexp/bbminigames/twtClassic/ability/Ability.java
@@ -0,0 +1,7 @@
+package gruvexp.bbminigames.twtClassic.ability;
+
+public class Ability { // each player has some ability objects
+
+    // everything that all abilities should have and all the methods to customize them
+
+}
