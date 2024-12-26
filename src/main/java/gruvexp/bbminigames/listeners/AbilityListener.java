b73e98787d4e2368866458bd5060e791d51e7199
GruveXp <gruveexp123@gmail.com>
2024-12-26 15:38:17 +0100

    fix: potion ability using should be own event

--- src/main/java/gruvexp/bbminigames/listeners/AbilityListener.java
+++ src/main/java/gruvexp/bbminigames/listeners/AbilityListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.EquipmentSlot;
 
 public class AbilityListener implements Listener {
@@ -25,11 +26,30 @@ public class AbilityListener implements Listener {
                 return;
             }
             switch (type) {
-                case ENDER_PEARL, SPEED_POTION, INVIS_POTION -> bp.getAbility(type).use();
+                case ENDER_PEARL -> bp.getAbility(type).use();
                 case WIND_CHARGE -> {
                     if (e.getItem().getAmount() == 1) bp.getAbility(type).use();
                 }
             }
+        }
+    }
+
+    @EventHandler
+    public void onPotionAbilityUse(PlayerItemConsumeEvent e) {
+        Player p = e.getPlayer();
+        BotBowsPlayer bp = BotBows.getBotBowsPlayer(p);
+        if (bp == null) return;
+        AbilityType type = AbilityType.fromItem(e.getItem());
+        if (type == null) return;
+        BotBows.debugMessage("Used ability: " + type.name());
+        if (!BotBows.activeGame) {
+            BotBows.debugMessage("Game is not active: cancelling");
+            e.setCancelled(true); // kanke bruke abilities i lobbyen
+            return;
+        }
+        BotBows.debugMessage("Ability gets used");
+        switch (type) {
+            case SPEED_POTION, INVIS_POTION -> bp.getAbility(type).use();
         }
     }
 
