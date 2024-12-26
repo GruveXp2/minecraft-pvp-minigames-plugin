f39e1e3aa545284a410ec9985e4ce9a4791b2f39
GruveXp <gruveexp123@gmail.com>
2024-12-26 22:25:10 +0100

    fix: armor still showed making the invis potion redundant

--- src/main/java/gruvexp/bbminigames/twtClassic/ability/abilities/InvisPotionAbility.java
+++ src/main/java/gruvexp/bbminigames/twtClassic/ability/abilities/InvisPotionAbility.java
@@ -1,13 +1,32 @@
 package gruvexp.bbminigames.twtClassic.ability.abilities;
 
+import gruvexp.bbminigames.Main;
 import gruvexp.bbminigames.twtClassic.BotBowsPlayer;
 import gruvexp.bbminigames.twtClassic.ability.Ability;
 import gruvexp.bbminigames.twtClassic.ability.AbilityType;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
 
 public class InvisPotionAbility extends Ability {
     public InvisPotionAbility(BotBowsPlayer player, int hotBarSlot) {
         super(player, hotBarSlot);
         this.type = AbilityType.INVIS_POTION;
         this.maxCooldown = 25;
+    }
+
+    @Override
+    public void use() {
+        super.use();
+        Inventory inv = player.player.getInventory();
+        for (int i = 0; i < 4; i++) { // flytter armor ned
+            inv.setItem(9 + i, inv.getItem(100 + i));
+            inv.setItem(100 + i, null);
+        }
+        Bukkit.getScheduler().runTaskLater(Main.getPlugin(), task -> {
+            for (int i = 0; i < 4; i++) { // flytter armor ned
+                inv.setItem(100 + i, inv.getItem(9 + i));
+                inv.setItem(9 + i, null);
+            }
+        }, 100L); // 5 sekunder
     }
 }
