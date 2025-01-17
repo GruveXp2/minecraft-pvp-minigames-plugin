8190365e6df2e724cc73489e5cfb5fe207880d72
GruveXp <gruveexp123@gmail.com>
2025-01-17 16:26:31 +0100

    changed warning intervals

--- src/main/java/gruvexp/bbminigames/tasks/RoundTimer.java
+++ src/main/java/gruvexp/bbminigames/tasks/RoundTimer.java
@@ -18,15 +18,22 @@ public class RoundTimer extends BukkitRunnable {
     @Override
     public void run() {
         switch (time) {
-            case 300, 240, 180, 120, 60 -> botBowsGame.lobby.messagePlayers(Component.text("Round ends in ")
+            case 300, 180, 120, 60 -> botBowsGame.lobby.messagePlayers(Component.text("Round ends in ")
                     .append(Component.text(time/60, NamedTextColor.YELLOW))
                     .append(Component.text(" minute" + (time == 60 ? "" : "s"))));
-            case 30, 20, 10, 5 -> botBowsGame.lobby.messagePlayers(Component.text("Round ends in ", NamedTextColor.YELLOW)
+            case 30, 10 -> botBowsGame.lobby.messagePlayers(Component.text("Round ends in ", NamedTextColor.YELLOW)
                     .append(Component.text(time, NamedTextColor.GOLD))
                     .append(Component.text(" seconds", NamedTextColor.YELLOW)));
             case 0 -> {
                 botBowsGame.endGameTimeout();
                 cancel(); // stopper loopen
+            }
+            default -> {
+                if (time % 300 == 0) {
+                    botBowsGame.lobby.messagePlayers(Component.text("Round ends in ")
+                        .append(Component.text(time/60))
+                        .append(Component.text(" minutes")));
+                }
             }
         }
         time--;
