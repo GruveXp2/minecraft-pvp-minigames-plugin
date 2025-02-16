697e0e9e8952a8b423c3cc46ad02f6cfa0d14d85
GruveXp <gruveexp123@gmail.com>
2025-02-16 20:16:36 +0100

    fix: invis ability didnt hidet he armor

--- src/main/java/gruvexp/bbminigames/twtClassic/ability/abilities/InvisPotionAbility.java
+++ src/main/java/gruvexp/bbminigames/twtClassic/ability/abilities/InvisPotionAbility.java
@@ -6,6 +6,8 @@ import gruvexp.bbminigames.twtClassic.ability.Ability;
 import gruvexp.bbminigames.twtClassic.ability.AbilityType;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class InvisPotionAbility extends Ability {
     public InvisPotionAbility(BotBowsPlayer player, int hotBarSlot) {
@@ -17,16 +19,17 @@ public class InvisPotionAbility extends Ability {
     @Override
     public void use() {
         super.use();
-        Inventory inv = player.player.getInventory();
-        for (int i = 0; i < 4; i++) { // flytter armor ned
-            inv.setItem(9 + i, inv.getItem(100 + i));
-            inv.setItem(100 + i, null);
-        }
+        PlayerInventory inv = player.player.getInventory();
+        inv.setItem(9, inv.getHelmet());
+        inv.setItem(10, inv.getChestplate());
+        inv.setItem(11, inv.getLeggings());
+        inv.setItem(12, inv.getBoots());
+        inv.setArmorContents(new ItemStack[] {null, null, null, null});
         Bukkit.getScheduler().runTaskLater(Main.getPlugin(), task -> {
             for (int i = 0; i < 4; i++) { // flytter armor ned
-                inv.setItem(100 + i, inv.getItem(9 + i));
                 inv.setItem(9 + i, null);
             }
+            inv.setArmorContents(new ItemStack[] {inv.getItem(9), inv.getItem(10), inv.getItem(11), inv.getItem(12)});
         }, 100L); // 5 sekunder
     }
 }
