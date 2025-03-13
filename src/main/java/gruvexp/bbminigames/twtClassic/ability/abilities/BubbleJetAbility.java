583b3a49b6d733111bbded841d34082ff3f9e991
David Alexander Danielsen <gruveexp123@gmail.com>
2025-03-13 23:09:02 +0100

    add bubble jet ability

---
+++ src/main/java/gruvexp/bbminigames/twtClassic/ability/abilities/BubbleJetAbility.java
@@ -0,0 +1,15 @@
+package gruvexp.bbminigames.twtClassic.ability.abilities;
+
+import gruvexp.bbminigames.twtClassic.BotBowsPlayer;
+import gruvexp.bbminigames.twtClassic.ability.Ability;
+import gruvexp.bbminigames.twtClassic.ability.AbilityType;
+
+public class BubbleJetAbility extends Ability {
+
+    public static final double DAMAGE_RADIUS = 2.0;
+
+    public BubbleJetAbility(BotBowsPlayer bp, int hotBarSlot) {
+        super(bp, hotBarSlot);
+        this.type = AbilityType.BUBBLE_JET;
+    }
+}
