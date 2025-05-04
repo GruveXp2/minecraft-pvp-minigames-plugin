6fd3f831bb3ddb1cd35872c2a16723843d6fd998
GruveXp <gruveexp123@gmail.com>
2025-05-04 18:20:18 +0200

    cleanup of the ability items desc and add useful info

--- src/main/java/gruvexp/bbminigames/twtClassic/ability/AbilityType.java
+++ src/main/java/gruvexp/bbminigames/twtClassic/ability/AbilityType.java
@@ -6,12 +6,15 @@ import gruvexp.bbminigames.twtClassic.ability.abilities.*;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -30,21 +33,24 @@ public enum AbilityType {
             "CONCRETE_POWDER", AbilityCategory.DAMAGING),
     THUNDER_BOW(Menu.makeItem(Material.BLUE_ICE, Component.text("Thunder Bow"),
             Component.text("Converts your crossbow into a thunder crossbow"),
-            Component.text("When hitting an enemy,"),
-            Component.text("lightning strikes other enemies within 6 blocks"),
-            getDurationComponent(ThunderBowAbility.DURATION)),
+            Component.text("When hitting an opponent, damage chains to nearby enemies"),
+            Component.empty(),
+            getDamageInfo("chain", 6, 'r'),
+            getDurationInfo(ThunderBowAbility.DURATION)),
             "TERRACOTTA", AbilityCategory.DAMAGING),
-    BUBBLE_JET(getRiptideTrident(),
+    BUBBLE_JET(makeRiptideTrident(),
             "CANDLE", AbilityCategory.DAMAGING),
-    LONG_ARMS(getLongHandsItem(),
+    LONG_ARMS(makeLongHandsItem(),
             "WOOL", AbilityCategory.DAMAGING),
     SALMON_SLAP(Menu.makeItem(Material.SALMON_BUCKET, Component.text("Salmon Slap"),
-            Component.text("Melee weapon"),
-            getDurationComponent(SalmonSlapAbility.DURATION)),
+            Component.text("Give your opponents a salmon slap"),
+            Component.empty(),
+            getDamageInfo("punch", 3, 'm'),
+            getDurationInfo(SalmonSlapAbility.DURATION)),
             "WOOL", AbilityCategory.DAMAGING),
     RADAR(Menu.makeItem(Material.BELL, Component.text("Radar"),
-            Component.text("Reveals the position of the enemy team by making them glow"),
-            getDurationComponent(RadarAbility.DURATION)),
+            Component.text("Reveals the position of opponents by making them glow"),
+            getDurationInfo(RadarAbility.DURATION)),
             30, "BANNER", AbilityCategory.UTILITY),
     ENDER_PEARL(Menu.makeItem(Material.ENDER_PEARL, Component.text("Ender Pearl")),
             15, "CONCRETE", AbilityCategory.UTILITY),
@@ -62,7 +68,7 @@ public enum AbilityType {
             25, "CONCRETE_POWDER", AbilityCategory.TRAP),
     LINGERING_POTION(makeLingeringPotion(),
             LingeringPotionAbility.DURATION + 5, "CANDLE", AbilityCategory.TRAP),
-    FLOAT_SPELL(getFloatSpellItem(),
+    FLOAT_SPELL(makeFloatSpellItem(),
             10, "BUNDLE", AbilityCategory.UTILITY),
     WIND_CHARGE(Menu.makeItem(Material.WIND_CHARGE, Component.text("Wind Charge"), 3),
             15, "DYE", AbilityCategory.UTILITY),
@@ -70,7 +76,7 @@ public enum AbilityType {
             25, "CANDLE", AbilityCategory.POTION),
     SHRINK(Menu.makeItem(Material.REDSTONE, Component.text("Shrink"),
             Component.text("Makes you shrink to half the size"),
-            getDurationComponent(5)),
+            getDurationInfo(5)),
             20, "BUNDLE", AbilityCategory.UTILITY);
 
     private final ItemStack abilityItem;
@@ -144,6 +150,28 @@ public enum AbilityType {
         return null;
     }
 
+    private static @NotNull TextComponent getDurationInfo(int seconds) {
+        return Component.text("Duration: ", NamedTextColor.DARK_GREEN)
+                .append(Component.text(seconds + "s", NamedTextColor.GREEN))
+                .decoration(TextDecoration.ITALIC, false);
+    }
+
+    private static final TextColor POTION_LABEL_COLOR = TextColor.color(0x0080ff);
+    private static final TextColor POTION_EFFECT_COLOR = TextColor.color(0x80c0ff);
+
+    private static @NotNull TextComponent getDamageInfo(String damageType, int value, char unit) {
+        return Component.text("Damage: ", NamedTextColor.RED)
+                .append(Component.text(damageType, NamedTextColor.GOLD)).appendSpace()
+                .append(Component.text(value + "" + unit, NamedTextColor.YELLOW))
+                .decoration(TextDecoration.ITALIC, false);
+    }
+
+    private static @NotNull TextComponent getPotionEffectInfo(String potionEffect) {
+        return Component.text("Potion effect: ", POTION_LABEL_COLOR)
+                .append(Component.text(potionEffect, POTION_EFFECT_COLOR))
+                .decoration(TextDecoration.ITALIC, false);
+    }
+
     private static ItemStack makePotion(PotionType type) {
         ItemStack potion = new ItemStack(Material.POTION);
         PotionMeta meta = (PotionMeta) potion.getItemMeta();
@@ -156,7 +184,8 @@ public enum AbilityType {
             default -> "Potion of unnamed 0";
         };
         meta.customName(Component.text(name));
-        meta.lore(List.of(getDurationComponent(5)));
+        meta.lore(List.of(getDurationInfo(5)));
+        meta.addItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
 
         potion.setItemMeta(meta);
         return potion;
@@ -168,9 +197,14 @@ public enum AbilityType {
 
         meta.addCustomEffect(new PotionEffect(PotionEffectType.SPEED, BabyPotionAbility.DURATION * 20, 4), true);
         meta.customName(Component.text("Baby Potion"));
-        meta.lore(List.of(Component.text("2x Speed", NamedTextColor.BLUE),
-                Component.text("-30% Size", NamedTextColor.BLUE),
-                getDurationComponent(BabyPotionAbility.DURATION)));
+        meta.lore(List.of(
+                Component.text("Makes you small and fast"),
+                Component.empty(),
+                getPotionEffectInfo("2x Speed"),
+                getPotionEffectInfo("-30% Size"),
+                getDurationInfo(BabyPotionAbility.DURATION)
+        ));
+        meta.addItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
 
         potion.setItemMeta(meta);
         return potion;
@@ -182,8 +216,13 @@ public enum AbilityType {
 
         meta.addCustomEffect(new PotionEffect(PotionEffectType.LUCK, ChargePotionAbility.DURATION * 20, 4), true);
         meta.customName(Component.text("Charge Potion"));
-        meta.lore(List.of(Component.text("2x cooldown reduction speed", NamedTextColor.BLUE),
-                getDurationComponent(ChargePotionAbility.DURATION)));
+        meta.lore(List.of(
+                Component.text("Makes your cooldowns go faster"),
+                Component.empty(),
+                getPotionEffectInfo("2x cooldown speed"),
+                getDurationInfo(ChargePotionAbility.DURATION)
+        ));
+        meta.addItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
 
         potion.setItemMeta(meta);
         return potion;
@@ -195,9 +234,14 @@ public enum AbilityType {
 
         meta.addCustomEffect(new PotionEffect(PotionEffectType.UNLUCK, KarmaPotion.DURATION * 20, 4), true);
         meta.customName(Component.text("Karma Potion"));
-        meta.lore(List.of(Component.text("Attacker gets glowing and", NamedTextColor.BLUE),
+        meta.lore(List.of(
+                Component.text("Attacker gets glowing and", NamedTextColor.BLUE),
                 Component.text("slowness, levitation, nausea, or blindness", NamedTextColor.BLUE),
-                getDurationComponent(KarmaPotion.DURATION)));
+                Component.empty(),
+                getPotionEffectInfo("karma"),
+                getDurationInfo(KarmaPotion.DURATION)
+        ));
+        meta.addItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
 
         potion.setItemMeta(meta);
         return potion;
@@ -207,13 +251,17 @@ public enum AbilityType {
         ItemStack potion = new ItemStack(Material.LINGERING_POTION);
         PotionMeta meta = (PotionMeta) potion.getItemMeta();
         meta.customName(Component.text("Lingering potion"));
-        meta.lore(List.of(Component.text("Contains one of the following at random:"),
+        meta.lore(List.of(
+                Component.text("Contains one of the following at random:"),
                 Component.text("Nausea", NamedTextColor.LIGHT_PURPLE),
                 Component.text("Slowness", NamedTextColor.LIGHT_PURPLE),
                 Component.text("Levitation", NamedTextColor.LIGHT_PURPLE),
                 Component.text("Blindness", NamedTextColor.LIGHT_PURPLE),
-                getDurationComponent(LingeringPotionAbility.DURATION)));
+                getDurationInfo(LingeringPotionAbility.DURATION)
+        ));
+        meta.addItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
         meta.setColor(Color.fromRGB(100, 62, 46));
+
         potion.setItemMeta(meta);
         return potion;
     }
@@ -222,28 +270,31 @@ public enum AbilityType {
         ItemStack splashBow = new ItemStack(Material.BOW);
         ItemMeta meta = splashBow.getItemMeta();
         meta.displayName(Component.text("Splash Bow"));
-        meta.lore(List.of(Component.text("A bow that does splash damage"), Component.text("in 3 blocks radius")));
+        meta.lore(List.of(
+                Component.text("A bow that shoots arrows exploding on impact"),
+                Component.empty(),
+                getDamageInfo("splash", 6, 'r')
+        ));
         meta.addEnchant(Enchantment.POWER, 10, true);
         meta.addEnchant(Enchantment.PUNCH, 10, true);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         Damageable damageable = (Damageable) meta;
         damageable.setDamage((short) 384);
         splashBow.setItemMeta(damageable);
         return splashBow;
     }
 
-    private static @NotNull TextComponent getDurationComponent(int seconds) {
-        return Component.text("Duration: ").append(Component.text(seconds + "s", NamedTextColor.GREEN));
-    }
-
-    private static ItemStack getFloatSpellItem() {
+    private static ItemStack makeFloatSpellItem() {
         ItemStack item = Menu.makeItem(Material.EGG, Component.text("Float Spell"));
-        item.lore(List.of(getDurationComponent(FloatSpellAbility.DURATION)));
+        item.lore(List.of(getDurationInfo(FloatSpellAbility.DURATION)));
         return item;
     }
 
-    private static ItemStack getLongHandsItem() {
+    private static ItemStack makeLongHandsItem() {
         ItemStack item = Menu.makeItem(Material.FISHING_ROD, Component.text("Cool Rod"),
-                Component.text("Punch someone far away, only 1 punch granted"));
+                Component.text("Punch someone far away, only 1 punch granted"),
+                Component.empty(),
+                getDamageInfo("punch", 50, 'm'));
         ItemMeta meta = item.getItemMeta();
 
         // Create a unique NamespacedKey for this item
@@ -259,10 +310,20 @@ public enum AbilityType {
         return item;
     }
 
-    private static ItemStack getRiptideTrident() {
-        ItemStack item = Menu.makeItem(Material.TRIDENT, Component.text("Trident"),
+    private static ItemStack makeRiptideTrident() {
+        ItemStack item = new ItemStack(Material.TRIDENT);
+        ItemMeta meta = item.getItemMeta();
+        meta.displayName(Component.text("Trident"));
+        meta.lore(List.of(
                 Component.text("Makes you fly thru the air"),
-                Component.text("and damage enemies in a 2m radius"));
+                Component.text("and damage opponents in a 2m radius"),
+                Component.empty(),
+                getDamageInfo("aura", 2, 'r')
+        ));
+        meta.setAttributeModifiers(null);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+
+        item.setItemMeta(meta);
         return item;
     }
 }
