c459275131be49cab36a5b6ed50e93a50d7190fe
GruveXp <gruveexp123@gmail.com>
2025-05-31 19:55:38 +0200

    add hatches to steampunk arena

--- src/main/java/gruvexp/bbminigames/twtClassic/botbowsGames/SteamPunkGame.java
+++ src/main/java/gruvexp/bbminigames/twtClassic/botbowsGames/SteamPunkGame.java
@@ -1,7 +1,9 @@
 package gruvexp.bbminigames.twtClassic.botbowsGames;
 
 import gruvexp.bbminigames.Main;
+import gruvexp.bbminigames.mechanics.Hatch;
 import gruvexp.bbminigames.mechanics.SteamPipe;
+import gruvexp.bbminigames.twtClassic.BotBows;
 import gruvexp.bbminigames.twtClassic.Settings;
 import gruvexp.bbminigames.twtClassic.botbowsTeams.BotBowsTeam;
 import org.bukkit.Axis;
@@ -11,11 +13,9 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class SteamPunkGame extends BotBowsGame {
 
@@ -23,9 +23,13 @@ public class SteamPunkGame extends BotBowsGame {
     private final HashMap<Chunk, Set<SteamPipe>> pipeChunks = new HashMap<>();
     private SteamPipeMotor steamPipeMotor; // responsible for powering the pipes by giving them 20 ticks/s
 
+    private final Set<Hatch> hatches = new HashSet<>();
+    private final Map<Hatch, HatchMotor> hatchMotors = new HashMap<>();
+
     public SteamPunkGame(Settings settings) {
         super(settings);
         World world = Main.WORLD;
+        // steam pipes
         // copper -> weathered
         registerSteamPipe(new SteamPipe(true, List.of(
                 new Location(world, -350.8, 17, -398.5),
@@ -83,7 +87,6 @@ public class SteamPunkGame extends BotBowsGame {
                 new Location(world, -342.5, 31, -358.5),
                 new Location(world, -347.5, 31, -358.5)
         ), Axis.Z, Axis.X));
-
         // surface_copper -> gate_right
         registerSteamPipe(new SteamPipe(false, List.of(
                 new Location(world, -333.8, 25, -382.5),
@@ -95,6 +98,55 @@ public class SteamPunkGame extends BotBowsGame {
                 new Location(world, -342.5, 31, -393.5),
                 new Location(world, -347.5, 31, -393.5)
         ), Axis.Z, Axis.X));
+
+        // surface_oxidized -> gate_left
+        registerSteamPipe(new SteamPipe(false, List.of(
+                new Location(world, -380.2, 25, -369.5),
+                new Location(world, -375.7, 25, -369.5),
+                new Location(world, -375.8, 25, -367.5),
+                new Location(world, -371.5, 25, -367.5),
+                new Location(world, -371.5, 25, -361.5),
+                new Location(world, -371.5, 31.9, -361.2),
+                new Location(world, -371.5, 31, -358.5),
+                new Location(world, -366.5, 31, -358.5)
+        ), Axis.Z, Axis.X));
+        // surface_oxidized -> gate_right
+        registerSteamPipe(new SteamPipe(false, List.of(
+                new Location(world, -380.2, 25, -382.5),
+                new Location(world, -375.7, 25, -382.5),
+                new Location(world, -375.8, 25, -384.5),
+                new Location(world, -371.5, 25, -384.5),
+                new Location(world, -371.5, 25, -390.5),
+                new Location(world, -371.5, 31.9, -390.8),
+                new Location(world, -371.5, 31, -393.5),
+                new Location(world, -366.5, 31, -393.5)
+        ), Axis.Z, Axis.X));
+
+        //hatches
+        //copper
+        hatches.add(new Hatch("steampunk_hatch_copper",
+                new Vector(-356, 21, -396),
+                new Vector(4, 3, 0),
+                new Vector(-357, 22, -396),
+                new Vector(0, 4, 3)));
+        //weathered
+        hatches.add(new Hatch("steampunk_hatch_weathered",
+                new Vector(-362, 21, -396),
+                new Vector(4, 3, 0),
+                new Vector(-358, 22, -396),
+                new Vector(0, 4, 3)));
+        //exposed
+        hatches.add(new Hatch("steampunk_hatch_exposed",
+                new Vector(-356, 21, -359),
+                new Vector(4, 3, 0),
+                new Vector(-357, 22, -359),
+                new Vector(0, 4, 3)));
+        //oxidized
+        hatches.add(new Hatch("steampunk_hatch_oxidized",
+                new Vector(-362, 21, -359),
+                new Vector(4, 3, 0),
+                new Vector(-358, 22, -359),
+                new Vector(0, 4, 3)));
     }
 
     private void registerSteamPipe(SteamPipe steamPipe) {
@@ -110,6 +162,19 @@ public class SteamPunkGame extends BotBowsGame {
         super.startRound();
         steamPipeMotor = new SteamPipeMotor(steamPipes);
         steamPipeMotor.runTaskTimer(Main.getPlugin(), 200L, 1L);
+        hatches.forEach(hatch -> {
+            hatchMotors.put(hatch, new HatchMotor(hatch));
+            scheduleHatch(hatch);
+        });
+    }
+
+    @Override
+    protected void postRound(BotBowsTeam winningTeam, int winScore) {
+        steamPipeMotor.cancel();
+        steamPipeMotor = null;
+        hatchMotors.values().forEach(BukkitRunnable::cancel);
+        hatchMotors.clear();
+        super.postRound(winningTeam, winScore);
     }
 
     @Override
@@ -123,11 +188,11 @@ public class SteamPunkGame extends BotBowsGame {
         }
     }
 
-    @Override
-    public void postGame(BotBowsTeam winningTeam) {
-        super.postGame(winningTeam);
-        steamPipeMotor.cancel();
-        steamPipeMotor = null;
+    private void scheduleHatch(Hatch hatch) {
+        int randomDelay = BotBows.RANDOM.nextInt(14) + 2; // they toggle each 2-16 seconds
+        HatchMotor motor = new HatchMotor(hatch);
+        motor.runTaskLater(Main.getPlugin(), randomDelay * 20);
+        hatchMotors.put(hatch, motor);
     }
 
 
@@ -142,6 +207,21 @@ public class SteamPunkGame extends BotBowsGame {
         @Override
         public void run() { // checks if a player is near the dungeon, doesn't scan that often to not waste resources
             steamPipes.forEach(SteamPipe::tick);
+        }
+    }
+
+    private class HatchMotor extends BukkitRunnable {
+
+        public final Hatch hatch;
+
+        public HatchMotor(Hatch hatch) {
+            this.hatch = hatch;
+        }
+
+        @Override
+        public void run() {
+            hatch.toggle();
+            scheduleHatch(hatch);
         }
     }
 }
