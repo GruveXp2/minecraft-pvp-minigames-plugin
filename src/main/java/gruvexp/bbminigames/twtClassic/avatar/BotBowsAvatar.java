5fd21a8b60a6d5c7a1e11df6af6fa7d44a11f864
GruveXp <gruveexp123@gmail.com>
2026-02-11 23:22:52 +0100

    add: BotBowsAvatar and PlayerAvatar to separate game logic from player representation (visuals, animations, etc)

---
+++ src/main/java/gruvexp/bbminigames/twtClassic/avatar/BotBowsAvatar.java
@@ -0,0 +1,9 @@
+package gruvexp.bbminigames.twtClassic.avatar;
+
+public interface BotBowsAvatar {
+    void eliminate();
+    void revive();
+    void setHP();
+    void setMaxHP();
+    
+}
---
+++ src/main/java/gruvexp/bbminigames/twtClassic/avatar/PlayerAvatar.java
@@ -0,0 +1,24 @@
+package gruvexp.bbminigames.twtClassic.avatar;
+
+public class PlayerAvatar implements BotBowsAvatar{
+
+    @Override
+    public void eliminate() {
+
+    }
+
+    @Override
+    public void revive() {
+
+    }
+
+    @Override
+    public void setHP() {
+
+    }
+
+    @Override
+    public void setMaxHP() {
+
+    }
+}
