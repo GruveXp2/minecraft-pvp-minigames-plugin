a0293402d11056a5ad6609fb2b3c2c0b868c470e
GruveXp <gruveexp123@gmail.com>
2026-03-10 15:29:04 +0100

    feat: implement dynamic death message formatting with placeholders

--- src/main/java/gruvexp/bbminigames/api/damage/DamageContext.kt
+++ src/main/java/gruvexp/bbminigames/api/damage/DamageContext.kt
@@ -3,32 +3,35 @@ package gruvexp.bbminigames.api.damage
 import gruvexp.bbminigames.twtClassic.BotBows
 import gruvexp.bbminigames.twtClassic.BotBowsPlayer
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
 
 sealed interface DamageContext {
     val type: DamageType
 
-    fun getDisplayColor(): TextColor
+    fun getMessageColor(): TextColor
 
     data class Player(val attacker: BotBowsPlayer, override val type: DamageType.Player) : DamageContext {
-        override fun getDisplayColor(): TextColor = attacker.teamColor // Her har vi tilgang til spilleren!
+        override fun getMessageColor(): TextColor = BotBows.lighten(attacker.teamColor, 0.5)
     }
 
     data class Environment(override val type: DamageType.Environment, val color: TextColor) : DamageContext {
-        override fun getDisplayColor(): TextColor = color // Her bruker vi fargen fra miljø-typen
+        override fun getMessageColor(): TextColor = color
     }
 
     fun formatMessage(defender: BotBowsPlayer): Component {
-        val attackerColor: TextColor = getDisplayColor()
-        val attackerColorLight = BotBows.lighten(attackerColor, 0.5)
-        var msg = Component.text("", attackerColorLight)
+        val baseColor = getMessageColor()
 
-        var template = type.template.replace("%d", defender.plainName)
+        val tokens = type.template.split(Regex("(?=%d|%a)|(?<=%d|%a)")) // the regex gets the indices of where to split (right before and after %d or %a), so it basically splits but keeps the %a and %d
+        var component = Component.text("", baseColor)
 
-        if (this is Player) {
-            template = template.replace("%a", attacker.plainName)
+        for (part in tokens) {
+            component = when (part) { // replaces %d and %a with defender and attacker name
+                "%d" -> component.append(defender.name)
+                "%a" -> component.append(if (this is Player) attacker.name else Component.text("<error: no attacker>", NamedTextColor.RED))
+                else -> component.append(Component.text(part))
+            }
         }
-
-        return msg
+        return component
     }
 }
