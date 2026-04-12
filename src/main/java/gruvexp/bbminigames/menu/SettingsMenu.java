7cafb61a0744d52ce1ce32f2b230893debe7573b
David Alexander Danielsen <davidada@uia.no>
2026-04-12 17:44:45 +0200

    small fix: remove unused import

--- src/main/java/gruvexp/bbminigames/menu/SettingsMenu.java
+++ src/main/java/gruvexp/bbminigames/menu/SettingsMenu.java
@@ -1,6 +1,5 @@
 package gruvexp.bbminigames.menu;
 
-import gruvexp.bbminigames.twtClassic.BotBows;
 import gruvexp.bbminigames.twtClassic.Settings;
 
 public abstract class SettingsMenu extends Menu {
