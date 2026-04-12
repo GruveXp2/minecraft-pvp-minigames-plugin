fc4eb42fdfdd7fdea60faab11025a9862888cb8b
David Alexander Danielsen <davidada@uia.no>
2026-04-12 10:31:13 +0200

    refactor: move menu map items to BotBowsMap enum

--- src/main/java/gruvexp/bbminigames/menu/menus/MapMenu.java
+++ src/main/java/gruvexp/bbminigames/menu/menus/MapMenu.java
@@ -7,7 +7,6 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
@@ -21,81 +20,6 @@ public class MapMenu extends SettingsMenu {
     public static final ItemStack MAP_CATEGORY_OLD = makeItem("gear", Component.text("Map category"),
             Component.text("Old BotBows", NamedTextColor.YELLOW),
             Component.text("2019-2020", NamedTextColor.GOLD));
-
-
-    public static final ItemStack CLASSIC_ARENA = makeItem(Material.SLIME_BALL, Component.text("Classic Arena", NamedTextColor.GRAY),
-            Component.text("Blaud", NamedTextColor.BLUE)
-                    .append(Component.text(" vs ", NamedTextColor.WHITE))
-                    .append(Component.text("Sauce", NamedTextColor.RED)),
-            Component.text("A flat arena with modern royal style"),
-            Component.text("Has a huge cave room underground"));
-
-    public static final ItemStack ICY_RAVINE = makeItem(Material.SPRUCE_SAPLING, Component.text("Icy Ravine", NamedTextColor.AQUA),
-            Component.text("Graut", NamedTextColor.LIGHT_PURPLE)
-                    .append(Component.text(" vs ", NamedTextColor.WHITE))
-                    .append(Component.text("Wacky", NamedTextColor.GREEN)),
-            Component.text("A flat arena in a spruce forest with ice spikes and igloos"),
-            Component.text("Has a huge ravine in the middle and many caves underground"));
-
-    public static final ItemStack ROYAL_CASTLE = makeItem(Material.STONE_BRICK_STAIRS, Component.text("Royal Castle", NamedTextColor.GREEN),
-            Component.text("Kjødd", NamedTextColor.DARK_AQUA)
-                    .append(Component.text(" vs ", NamedTextColor.WHITE))
-                    .append(Component.text("Goofy", NamedTextColor.DARK_GREEN)),
-            Component.text("A castle themed arena"));
-
-    public static final ItemStack STEAMPUNK = makeItem(Material.COPPER_BULB, Component.text("Steampunk", NamedTextColor.GOLD),
-            Component.text("Blocc", NamedTextColor.GOLD)
-                    .append(Component.text(" vs ", NamedTextColor.WHITE))
-                    .append(Component.text("Quicc", NamedTextColor.AQUA)),
-            Component.text("A steampunk themed arena"));
-
-    public static final ItemStack PIGLIN_HIDEOUT = makeItem(Material.MAGMA_BLOCK, Component.text("Piglin Hideout", NamedTextColor.RED),
-            Component.text("Piglin", NamedTextColor.GOLD)
-                    .append(Component.text(" vs ", NamedTextColor.WHITE))
-                    .append(Component.text("Hoglin", NamedTextColor.YELLOW)),
-            Component.text("A large volcano arena"));
-
-    public static final ItemStack INSIDE_BOTBASE = makeItem(Material.GREEN_GLAZED_TERRACOTTA, Component.text("Inside the BotBase", NamedTextColor.GREEN),
-            Component.text("Corner", NamedTextColor.GRAY)
-                    .append(Component.text(" vs ", NamedTextColor.WHITE))
-                    .append(Component.text("Core", NamedTextColor.GREEN)),
-            Component.text("Inside the BotBase building, with lots of"),
-            Component.text("wires, batteries, and electricity"));
-
-    public static final ItemStack OUTSIDE_BOTBASE = makeItem(Material.GRASS_BLOCK, Component.text("Outside the BotBase", NamedTextColor.GREEN),
-            Component.text("Core", NamedTextColor.GREEN)
-                    .append(Component.text(" vs ", NamedTextColor.WHITE))
-                    .append(Component.text("Mountain", NamedTextColor.AQUA)),
-            Component.text("A field outside the BotBase"),
-            Component.text("next to a mountain"));
-
-    public static final ItemStack ROCKET_FOREST = makeItem(Material.SPRUCE_SAPLING, Component.text("Rocket Forest", NamedTextColor.DARK_GREEN),
-            Component.text("Door", NamedTextColor.GRAY)
-                    .append(Component.text(" vs ", NamedTextColor.WHITE))
-                    .append(Component.text("Tunnel", NamedTextColor.DARK_GREEN)),
-            Component.text("In the Rocket Forest next to the mountain"),
-            Component.text("with a rocket launcher in the middle"));
-
-    public static final ItemStack ROCKET = makeItem(Material.CRAFTER, Component.text("Inside the Rocket", NamedTextColor.RED),
-            Component.text("Dropper", NamedTextColor.BLACK)
-                    .append(Component.text(" vs ", NamedTextColor.WHITE))
-                    .append(Component.text("Engine", NamedTextColor.RED)),
-            Component.text("Inside the Rocket, including the engine,"),
-            Component.text("control panel, and power supply"));
-
-    public static final ItemStack SPACE_STATION = makeItem(Material.GLASS, Component.text("Space Station", NamedTextColor.AQUA),
-            Component.text("Warm", NamedTextColor.RED)
-                    .append(Component.text(" vs ", NamedTextColor.WHITE))
-                    .append(Component.text("Cold", NamedTextColor.AQUA)),
-            Component.text("At the space station where you can"),
-            Component.text("traverse space tubes in low gravity"));
-
-    public static final ItemStack MARS = makeItem(Material.RED_SAND, Component.text("Mars Base", NamedTextColor.GOLD),
-            Component.text("???", NamedTextColor.GRAY)
-                    .append(Component.text(" vs ", NamedTextColor.WHITE))
-                    .append(Component.text("???", NamedTextColor.GRAY)),
-            Component.text("At the mars base. Sadly not finished yet,"),
-            Component.text("if it ever will be..."));
 
     private boolean isOldMapCategory = false;
 
@@ -127,7 +51,7 @@ public class MapMenu extends SettingsMenu {
             }
             case MAGMA_BLOCK -> settings.setMap(BotBowsMap.PIGLIN_HIDEOUT);
             case COPPER_BULB -> settings.setMap(BotBowsMap.STEAMPUNK);
-            case STONE_BRICK_STAIRS -> settings.setMap(BotBowsMap.ROYAL_MAP);
+            case STONE_BRICK_STAIRS -> settings.setMap(BotBowsMap.ROYAL_CASTLE);
             case RED_SAND -> clicker.sendMessage(Component.text("This map is not added yet", NamedTextColor.RED));
 
             case GREEN_GLAZED_TERRACOTTA -> settings.setMap(BotBowsMap.INSIDE_BOTBASE);
@@ -153,21 +77,21 @@ public class MapMenu extends SettingsMenu {
 
     public void updateMenu() {
         if (isOldMapCategory) {
-            inventory.setItem(1, INSIDE_BOTBASE);
-            inventory.setItem(2, OUTSIDE_BOTBASE);
-            inventory.setItem(3, ROCKET_FOREST);
+            inventory.setItem(1, BotBowsMap.INSIDE_BOTBASE.getMenuItem());
+            inventory.setItem(2, BotBowsMap.OUTSIDE_BOTBASE.getMenuItem());
+            inventory.setItem(3, BotBowsMap.ROCKET_FOREST.getMenuItem());
             inventory.setItem(4, VOID);
-            inventory.setItem(5, ROCKET);
-            inventory.setItem(6, SPACE_STATION);
-            inventory.setItem(7, MARS);
+            inventory.setItem(5, BotBowsMap.ROCKET.getMenuItem());
+            inventory.setItem(6, BotBowsMap.SPACE_STATION.getMenuItem());
+            inventory.setItem(7, BotBowsMap.MARS_BASE.getMenuItem());
             inventory.setItem(13, MAP_CATEGORY_OLD);
         } else {
             inventory.setItem(1, VOID);
-            inventory.setItem(2, CLASSIC_ARENA);
-            inventory.setItem(3, ICY_RAVINE);
-            inventory.setItem(4, ROYAL_CASTLE);
-            inventory.setItem(5, STEAMPUNK);
-            inventory.setItem(6, PIGLIN_HIDEOUT);
+            inventory.setItem(2, BotBowsMap.CLASSIC_ARENA.getMenuItem());
+            inventory.setItem(3, BotBowsMap.ICY_RAVINE.getMenuItem());
+            inventory.setItem(4, BotBowsMap.ROYAL_CASTLE.getMenuItem());
+            inventory.setItem(5, BotBowsMap.STEAMPUNK.getMenuItem());
+            inventory.setItem(6, BotBowsMap.PIGLIN_HIDEOUT.getMenuItem());
             inventory.setItem(7, VOID);
             inventory.setItem(13, MAP_CATEGORY_MODERN);
         }
--- src/main/java/gruvexp/bbminigames/twtClassic/BotBowsMap.java
+++ src/main/java/gruvexp/bbminigames/twtClassic/BotBowsMap.java
@@ -1,24 +1,107 @@
 package gruvexp.bbminigames.twtClassic;
 
 import com.google.common.collect.ImmutableSet;
+import gruvexp.bbminigames.menu.Menu;
 import gruvexp.bbminigames.twtClassic.hazard.HazardType;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 public enum BotBowsMap {
-    CLASSIC_ARENA(ImmutableSet.of(HazardType.STORM, HazardType.EARTHQUAKE, HazardType.GHOST)),
-    ICY_RAVINE(ImmutableSet.of(HazardType.STORM, HazardType.EARTHQUAKE, HazardType.GHOST)),
-    ROYAL_MAP(ImmutableSet.of(HazardType.GHOST)),
-    STEAMPUNK(ImmutableSet.of(HazardType.STORM, HazardType.EARTHQUAKE, HazardType.GHOST)),
-    PIGLIN_HIDEOUT(ImmutableSet.of(HazardType.EARTHQUAKE, HazardType.GHOST)),
+    CLASSIC_ARENA(ImmutableSet.of(HazardType.STORM, HazardType.EARTHQUAKE, HazardType.GHOST),
+            Menu.makeItem(Material.SLIME_BALL, Component.text("Classic Arena", NamedTextColor.GRAY),
+                    Component.text("Blaud", NamedTextColor.BLUE)
+                            .append(Component.text(" vs ", NamedTextColor.WHITE))
+                            .append(Component.text("Sauce", NamedTextColor.RED)),
+                    Component.text("A flat arena with modern royal style"),
+                    Component.text("Has a huge cave room underground"))),
 
-    INSIDE_BOTBASE(ImmutableSet.of(HazardType.STORM, HazardType.GHOST)),
-    OUTSIDE_BOTBASE(ImmutableSet.of(HazardType.STORM, HazardType.GHOST)),
-    ROCKET_FOREST(ImmutableSet.of(HazardType.STORM, HazardType.GHOST)),
-    ROCKET(ImmutableSet.of(HazardType.GHOST)),
-    SPACE_STATION(ImmutableSet.of(HazardType.GHOST));
+    ICY_RAVINE(ImmutableSet.of(HazardType.STORM, HazardType.EARTHQUAKE, HazardType.GHOST),
+            Menu.makeItem(Material.SPRUCE_SAPLING, Component.text("Icy Ravine", NamedTextColor.AQUA),
+                    Component.text("Graut", NamedTextColor.LIGHT_PURPLE)
+                            .append(Component.text(" vs ", NamedTextColor.WHITE))
+                            .append(Component.text("Wacky", NamedTextColor.GREEN)),
+                    Component.text("A flat arena in a spruce forest with ice spikes and igloos"),
+                    Component.text("Has a huge ravine in the middle and many caves underground"))),
+
+    ROYAL_CASTLE(ImmutableSet.of(HazardType.GHOST),
+            Menu.makeItem(Material.STONE_BRICK_STAIRS, Component.text("Royal Castle", NamedTextColor.GREEN),
+                    Component.text("Kjødd", NamedTextColor.DARK_AQUA)
+                            .append(Component.text(" vs ", NamedTextColor.WHITE))
+                            .append(Component.text("Goofy", NamedTextColor.DARK_GREEN)),
+                    Component.text("A castle themed arena"))),
+    STEAMPUNK(ImmutableSet.of(HazardType.STORM, HazardType.EARTHQUAKE, HazardType.GHOST),
+            Menu.makeItem(Material.COPPER_BULB, Component.text("Steampunk", NamedTextColor.GOLD),
+                    Component.text("Blocc", NamedTextColor.GOLD)
+                            .append(Component.text(" vs ", NamedTextColor.WHITE))
+                            .append(Component.text("Quicc", NamedTextColor.AQUA)),
+                    Component.text("A steampunk themed arena"))),
+
+    PIGLIN_HIDEOUT(ImmutableSet.of(HazardType.EARTHQUAKE, HazardType.GHOST),
+            Menu.makeItem(Material.MAGMA_BLOCK, Component.text("Piglin Hideout", NamedTextColor.RED),
+                    Component.text("Piglin", NamedTextColor.GOLD)
+                            .append(Component.text(" vs ", NamedTextColor.WHITE))
+                            .append(Component.text("Hoglin", NamedTextColor.YELLOW)),
+                    Component.text("A large volcano arena"))),
+
+    INSIDE_BOTBASE(ImmutableSet.of(HazardType.STORM, HazardType.GHOST),
+            Menu.makeItem(Material.GREEN_GLAZED_TERRACOTTA, Component.text("Inside the BotBase", NamedTextColor.GREEN),
+                    Component.text("Corner", NamedTextColor.GRAY)
+                            .append(Component.text(" vs ", NamedTextColor.WHITE))
+                            .append(Component.text("Core", NamedTextColor.GREEN)),
+                    Component.text("Inside the BotBase building, with lots of"),
+                    Component.text("wires, batteries, and electricity"))),
+
+    OUTSIDE_BOTBASE(ImmutableSet.of(HazardType.STORM, HazardType.GHOST),
+            Menu.makeItem(Material.GRASS_BLOCK, Component.text("Outside the BotBase", NamedTextColor.GREEN),
+                    Component.text("Core", NamedTextColor.GREEN)
+                            .append(Component.text(" vs ", NamedTextColor.WHITE))
+                            .append(Component.text("Mountain", NamedTextColor.AQUA)),
+                    Component.text("A field outside the BotBase"),
+                    Component.text("next to a mountain"))),
+
+    ROCKET_FOREST(ImmutableSet.of(HazardType.STORM, HazardType.GHOST),
+            Menu.makeItem(Material.SPRUCE_SAPLING, Component.text("Rocket Forest", NamedTextColor.DARK_GREEN),
+                    Component.text("Door", NamedTextColor.GRAY)
+                            .append(Component.text(" vs ", NamedTextColor.WHITE))
+                            .append(Component.text("Tunnel", NamedTextColor.DARK_GREEN)),
+                    Component.text("In the Rocket Forest next to the mountain"),
+                    Component.text("with a rocket launcher in the middle"))),
+
+    ROCKET(ImmutableSet.of(HazardType.GHOST),
+            Menu.makeItem(Material.CRAFTER, Component.text("Inside the Rocket", NamedTextColor.RED),
+                    Component.text("Dropper", NamedTextColor.BLACK)
+                            .append(Component.text(" vs ", NamedTextColor.WHITE))
+                            .append(Component.text("Engine", NamedTextColor.RED)),
+                    Component.text("Inside the Rocket, including the engine,"),
+                    Component.text("control panel, and power supply"))),
+
+    SPACE_STATION(ImmutableSet.of(HazardType.GHOST),
+            Menu.makeItem(Material.GLASS, Component.text("Space Station", NamedTextColor.AQUA),
+                    Component.text("Warm", NamedTextColor.RED)
+                            .append(Component.text(" vs ", NamedTextColor.WHITE))
+                            .append(Component.text("Cold", NamedTextColor.AQUA)),
+                    Component.text("At the space station where you can"),
+                    Component.text("traverse space tubes in low gravity"))),
+
+    MARS_BASE(ImmutableSet.of(HazardType.STORM, HazardType.GHOST),
+            Menu.makeItem(Material.RED_SAND, Component.text("Mars Base", NamedTextColor.GOLD),
+                    Component.text("???", NamedTextColor.GRAY)
+                            .append(Component.text(" vs ", NamedTextColor.WHITE))
+                            .append(Component.text("???", NamedTextColor.GRAY)),
+                    Component.text("At the mars base. Sadly not finished yet,"),
+                    Component.text("if it ever will be...")));
 
     public final ImmutableSet<HazardType> allowedHazards;
+    private final ItemStack menuItem;
 
-    BotBowsMap(ImmutableSet<HazardType> allowedHazards) {
+    BotBowsMap(ImmutableSet<HazardType> allowedHazards, ItemStack menuItem) {
         this.allowedHazards = allowedHazards;
+        this.menuItem = menuItem;
+    }
+
+    public ItemStack getMenuItem() {
+        return menuItem.clone();
     }
 }
