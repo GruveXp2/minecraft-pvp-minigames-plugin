16c1c14b66a99efc89cd00a7d38ee50fe941333f
Gruve <gruveexp123@gmail.com>
2026-07-08 20:46:12 +0200

    add: start work on PlayerEffectManager.kt

---
+++ src/main/java/gruvexp/bbminigames/twtClassic/effect/PlayerEffectManager.kt
@@ -0,0 +1,5 @@
+package gruvexp.bbminigames.twtClassic.effect
+
+class PlayerEffectManager {
+    // TODO: se i chatten for plan. gloweffect og sizeeffect enums, med prio. metheds for hver av de. et sted der det tickes. stopper loop når matchen er ferdig elle runda
+}
