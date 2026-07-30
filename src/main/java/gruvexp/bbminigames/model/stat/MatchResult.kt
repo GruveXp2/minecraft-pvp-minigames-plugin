663f5d72fa61debbb108049842c675c56298b880
David Alexander Danielsen <davidada@uia.no>
2026-07-30 20:28:26 +0200

    start work on ResultDisplay.kt

--- src/main/java/gruvexp/bbminigames/model/stat/MatchResult.kt
+++ src/main/java/gruvexp/bbminigames/model/stat/MatchResult.kt
@@ -1,8 +1,8 @@
 package gruvexp.bbminigames.model.stat
 
-import gruvexp.bbminigames.twtClassic.BotBowsMap
 import gruvexp.bbminigames.twtClassic.BotBowsPlayer
 import gruvexp.bbminigames.twtClassic.ability.AbilityType
+import gruvexp.bbminigames.twtClassic.map.BotBowsMap
 import java.time.LocalDateTime
 import java.util.UUID
 
---
+++ src/main/java/gruvexp/bbminigames/model/stat/ResultDisplay.kt
@@ -0,0 +1,11 @@
+package gruvexp.bbminigames.model.stat
+
+import org.bukkit.Location
+
+class ResultDisplay(loc: Location, matchResult: MatchResult) {
+
+    fun spawn() {
+
+    }
+
+}
