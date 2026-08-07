73a3e90aba12dda672e1872ca5aaa162b5309420
Gruve <gruveexp123@gmail.com>
2026-08-08 00:36:25 +0200

    migrate MenuCommand to Kotlin

--- src/main/java/gruvexp/bbminigames/commands/MenuCommand.kt
+++ src/main/java/gruvexp/bbminigames/commands/MenuCommand.kt
@@ -1,19 +1,17 @@
-package gruvexp.bbminigames.commands;
+package gruvexp.bbminigames.commands
 
-import gruvexp.bbminigames.twtClassic.BotBows;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
+import gruvexp.bbminigames.twtClassic.BotBows
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
-public class MenuCommand implements CommandExecutor {
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        if (!(sender instanceof Player p)) {
-            return false;
+class MenuCommand : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
+        if (sender !is Player) {
+            return false
         }
-        BotBows.gameMenu.open(p);
-        return true;
+        BotBows.gameMenu.open(sender)
+        return true
     }
 }
