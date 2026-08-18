f75052fa855b5c3d486015621926be85fe39c5cc
David Alexander Danielsen <davidada@uia.no>
2026-08-18 18:58:44 +0200

    refactor: migrate BubbleJet ability to Kotlin

--- src/main/java/gruvexp/bbminigames/twtClassic/ability/abilities/BubbleJet.kt
+++ src/main/java/gruvexp/bbminigames/twtClassic/ability/abilities/BubbleJet.kt
@@ -1,48 +1,42 @@
-package gruvexp.bbminigames.twtClassic.ability.abilities;
+package gruvexp.bbminigames.twtClassic.ability.abilities
 
-import gruvexp.bbminigames.Main;
-import gruvexp.bbminigames.api.damage.DamageContext;
-import gruvexp.bbminigames.api.damage.DamageType;
-import gruvexp.bbminigames.twtClassic.BotBowsPlayer;
-import gruvexp.bbminigames.twtClassic.ability.Ability;
-import gruvexp.bbminigames.twtClassic.ability.AbilityType;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
-import org.bukkit.scheduler.BukkitRunnable;
+import gruvexp.bbminigames.Main
+import gruvexp.bbminigames.api.damage.DamageContext
+import gruvexp.bbminigames.api.damage.DamageType
+import gruvexp.bbminigames.twtClassic.BotBowsPlayer
+import gruvexp.bbminigames.twtClassic.ability.Ability
+import gruvexp.bbminigames.twtClassic.ability.AbilityType
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+import org.bukkit.scheduler.BukkitRunnable
 
-public class BubbleJet extends Ability {
+class BubbleJet(bp: BotBowsPlayer, hotBarSlot: Int) : Ability(bp, hotBarSlot, AbilityType.BUBBLE_JET) {
+    var riptideTask: BukkitRunnable? = null
 
-    public static final double DAMAGE_RADIUS = 2.0;
-
-    BukkitRunnable riptideTask;
-
-    public BubbleJet(BotBowsPlayer bp, int hotBarSlot) {
-        super(bp, hotBarSlot, AbilityType.BUBBLE_JET);
-    }
-
-    @Override
-    public void use() {
-        super.use();
-        bp.setInvulnerable(true);
-        if (riptideTask != null) return;
-        riptideTask = new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (bp.isOnGround()) {
-                    bp.setInvulnerable(false);
-                    this.cancel(); // if the player is done riptiding and hitting the ground
-                    riptideTask = null;
-                    return;
+    override fun use() {
+        super.use()
+        bp.setInvulnerable(true)
+        if (riptideTask != null) return
+        riptideTask = object : BukkitRunnable() {
+            override fun run() {
+                if (bp.isOnGround) {
+                    bp.setInvulnerable(false)
+                    this.cancel() // if the player is done riptiding and hitting the ground
+                    riptideTask = null
+                    return
                 }
                 bp.getNearbyPlayers(DAMAGE_RADIUS).stream()
-                        .filter(nearbyPlayer -> nearbyPlayer.getTeam() != bp.getTeam())
-                        .forEach(target -> {
-                            target.avatar.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 60, 1, true, false));
-                            target.damage(new DamageContext.Player(DamageType.Player.BUBBLE_JET, bp));
-                            registerSuccess();
-                        });
+                    .filter { it.team != bp.team }
+                    .forEach {
+                        it.avatar.addPotionEffect(PotionEffect(PotionEffectType.LEVITATION, 60, 1, true, false))
+                        it.damage(DamageContext.Player(DamageType.Player.BUBBLE_JET, bp))
+                        registerSuccess()
+                    }
             }
-        };
-        riptideTask.runTaskTimer(Main.getPlugin(), 0L, 2L);
+        }.apply { runTaskTimer(Main.getPlugin(), 0L, 2L) }
+    }
+
+    companion object {
+        const val DAMAGE_RADIUS: Double = 2.0
     }
 }
