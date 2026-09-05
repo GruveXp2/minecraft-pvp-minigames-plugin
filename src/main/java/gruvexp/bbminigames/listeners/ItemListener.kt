a89881d50bceb2804e6f7260de7bba9e4b4caa8d
Gruve <gruveexp123@gmail.com>
2026-09-05 15:04:39 +0200

    refactor: migrate ItemListener to Kotlin

--- src/main/java/gruvexp/bbminigames/listeners/ItemListener.kt
+++ src/main/java/gruvexp/bbminigames/listeners/ItemListener.kt
@@ -1,18 +1,13 @@
-package gruvexp.bbminigames.listeners;
+package gruvexp.bbminigames.listeners
 
-import gruvexp.bbminigames.twtClassic.BotBows;
-import gruvexp.bbminigames.twtClassic.BotBowsPlayer;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerDropItemEvent;
+import gruvexp.bbminigames.twtClassic.BotBows
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerDropItemEvent
 
-public class ItemListener implements Listener {
-
+class ItemListener : Listener {
     @EventHandler
-    public void onItemDrop(PlayerDropItemEvent e) {
-        Player p = e.getPlayer();
-        BotBowsPlayer bp = BotBows.getBotBowsPlayer(p);
-        if (bp != null) e.setCancelled(true);
+    fun onItemDrop(e: PlayerDropItemEvent) {
+        if (BotBows.getBotBowsPlayer(e.player) != null) e.isCancelled = true
     }
 }
