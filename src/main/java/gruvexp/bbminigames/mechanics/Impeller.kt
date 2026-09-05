6cda4279b726287660fefb4ba895b9cc4e5ca176
Gruve <gruveexp123@gmail.com>
2026-09-05 18:02:11 +0200

    refactor: migrate Impeller to Kotlin

--- src/main/java/gruvexp/bbminigames/mechanics/Impeller.kt
+++ src/main/java/gruvexp/bbminigames/mechanics/Impeller.kt
@@ -1,102 +1,107 @@
-package gruvexp.bbminigames.mechanics;
+package gruvexp.bbminigames.mechanics
 
-import gruvexp.bbminigames.Util;
-import gruvexp.bbminigames.twtClassic.BotBows;
-import org.bukkit.Chunk;
-import org.bukkit.Location;
-import org.bukkit.block.structure.StructureRotation;
-import org.bukkit.entity.BlockDisplay;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.bukkit.structure.Structure;
-import org.bukkit.util.Vector;
+import gruvexp.bbminigames.Util
+import gruvexp.bbminigames.twtClassic.BotBows
+import org.bukkit.Chunk
+import org.bukkit.Location
+import org.bukkit.block.structure.StructureRotation
+import org.bukkit.entity.BlockDisplay
+import org.bukkit.entity.Player
+import org.bukkit.util.Vector
+import kotlin.math.atan2
+import kotlin.math.sqrt
 
-import java.util.HashSet;
-import java.util.Set;
+class Impeller(
+    id: Int,
+    structureName: String,
+    private val centerLocation: Location,
+    private val rotationSpeed: Float
+) {
+    private val displays = mutableSetOf<BlockDisplay>()
 
-public class Impeller {
+    private var jaw = 0f
+    private val players = mutableSetOf<Player>()
 
-    private final Set<BlockDisplay> displays;
-    private final Location centerLocation;
-    private final float rotationSpeed;
-
-    private float jaw = 0f;
-    private final Set<Player> players = new HashSet<>();
-
-    public Impeller(int id, String structureName, Location centerLocation, float rotationSpeed) {
-        this.centerLocation = centerLocation;
-        this.rotationSpeed = rotationSpeed;
-        displays = new HashSet<>();
-        for (Entity nearbyEntity : centerLocation.getNearbyEntities(2, 2, 2)) {
-            if (!(nearbyEntity instanceof BlockDisplay display)) continue;
-            if (!display.getScoreboardTags().contains(structureName + "_" + id)) continue;
-
-            displays.add(display);
-            display.setRotation(0, 0);
-        }
+    init {
+        centerLocation.getNearbyEntities(2.0, 2.0, 2.0)
+            .filterIsInstance<BlockDisplay>()
+            .filter { "${structureName}_$id" in it.scoreboardTags }
+            .forEach {
+                it.setRotation(0f, 0f)
+                displays.add(it)
+            }
         if (displays.isEmpty()) {
-            Structure structure = BotBows.loadStructure(structureName);
-            if (structure == null) return;
-
-            Location originLoc = centerLocation.clone().add(-2, 0, -2);
-            BotBows.placeSymmetricalStructure(structure, originLoc, centerLocation.clone().add(0.5, 0.5, 0.5), StructureRotation.NONE, 5, structureName + "_" + id, displays);
+            BotBows.loadStructure(structureName)?.let { structure ->
+                val originLoc = centerLocation.clone().add(-2.0, 0.0, -2.0)
+                BotBows.placeSymmetricalStructure(
+                    structure,
+                    originLoc,
+                    centerLocation.clone().add(0.5, 0.5, 0.5),
+                    StructureRotation.NONE,
+                    5,
+                    "${structureName}_$id",
+                    displays
+                )
+            }
         }
     }
 
-    // the chunks close to this impeller, only check for players if players are in these chunks
-    public Set<Chunk> getTickedChunks() {
-        return Util.getChunksAround(centerLocation, 3);
-    }
+    val tickedChunks: Set<Chunk>
+        // the chunks close to this impeller, only check for players if players are in these chunks
+        get() = Util.getChunksAround(centerLocation, 3)
 
-    public void checkProximity(Player p) {
-        if (players.contains(p)) return;
+    fun checkProximity(p: Player) {
+        if (p in players) return
 
-        if (p.getLocation().distanceSquared(centerLocation) < 9) {
-            players.add(p);
+        if (p.location.distanceSquared(centerLocation) < 9) { // radius 3
+            players.add(p)
         }
     }
 
-    public void tick() {
-        jaw += rotationSpeed;
+    fun tick() {
+        jaw += rotationSpeed
         if (jaw < 0) {
-            jaw += 360;
+            jaw += 360f
         } else if (jaw > 360) {
-            jaw = jaw % 360;
+            jaw %= 360
         }
-        displays.forEach(display -> display.setRotation(jaw, 0));
+        displays.forEach { it.setRotation(jaw, 0f) }
 
-        players.forEach(p -> {
-            Location pLoc = p.getLocation();
-            Vector relDir = centerLocation.clone().subtract(pLoc).toVector(); // retningsvektoren
-            relDir.setY(0);
-            double x = relDir.getX();
-            double z = relDir.getZ();
+        players.forEach { p ->
+            val pLoc = p.location
+            val relDir = centerLocation.clone().subtract(pLoc).toVector() // retningsvektoren
+            relDir.y = 0.0
+            val x = relDir.x
+            val z = relDir.z
             // polar form
-            double r = Math.sqrt(x * x + z * z);
-            double θ = Math.atan2(z, x);
+            val r = sqrt(x * x + z * z)
+            val θ = atan2(z, x)
 
-            double pJaw = Math.toDegrees(θ) - 90;
-            pJaw -= jaw; // relative to the rotation of the impeller
+            var pJaw = Math.toDegrees(θ) - 90
+            pJaw -= jaw // relative to the rotation of the impeller
             // make into interval [0,90)
-            pJaw = (pJaw + 720) % 90;
-            if (rotationSpeed > 0 ? pJaw < 20 : pJaw > 70) { // they hit the impeller and will get pushed
-                double divide = 5 + (rotationSpeed > 0 ? pJaw : 90 - pJaw) * 7;
-                Vector push = relDir.crossProduct(new Vector(0, -rotationSpeed, 0)).multiply(r/(divide)); // retning x up||down = vel from hitting the bl8d
+            pJaw = (pJaw + 720) % 90
+            if (if (rotationSpeed > 0) pJaw < 20 else pJaw > 70) { // they hit the impeller and will get pushed
+                val divide = 5 + (if (rotationSpeed > 0) pJaw else 90 - pJaw) * 7
+                val push = relDir.crossProduct(Vector(0f, -rotationSpeed, 0f))
+                    .multiply(r / divide) // retning x up||down = vel from hitting the bl8d
 
-                Vector v = p.getVelocity();
-                Vector pushDir = push.clone().normalize();
-                double vAlongPushDir = v.dot(pushDir);
-                v.subtract(pushDir.multiply(vAlongPushDir)).add(push); // the part of v in the direction pushed gets completly replaced with the push value
-                p.setVelocity(v);
-            } else if (rotationSpeed > 0 ? pJaw > 70 : pJaw < 20) { // they hit the impeller and will get pushed
-                Vector dv = relDir.crossProduct(new Vector(0, rotationSpeed, 0)).multiply(r/25); // retning x up||down = vel from hitting the bl8d
-                Vector v = p.getVelocity();
-                v.add(dv);
-                p.setVelocity(v);
+                val v = p.velocity
+                val pushDir = push.clone().normalize()
+                val vAlongPushDir = v.dot(pushDir)
+                v.subtract(pushDir.multiply(vAlongPushDir))
+                    .add(push) // the part of v in the direction pushed gets completly replaced with the push value
+                p.velocity = v
+            } else if (if (rotationSpeed > 0) pJaw > 70 else pJaw < 20) { // they hit the impeller and will get pushed
+                val dv = relDir.crossProduct(Vector(0f, rotationSpeed, 0f))
+                    .multiply(r / 25) // retning x up||down = vel from hitting the bl8d
+                val v = p.velocity
+                v.add(dv)
+                p.velocity = v
             }
             if (r > 3.1) {
-                players.remove(p);
+                players.remove(p)
             }
-        });
+        }
     }
 }
