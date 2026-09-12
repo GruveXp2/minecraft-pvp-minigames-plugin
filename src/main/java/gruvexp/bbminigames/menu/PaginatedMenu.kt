0845c83c055e128bdedce71172ee9e09fcbab94e
Gruve <gruveexp123@gmail.com>
2026-09-12 11:33:25 +0200

    refactor: migrate PaginatedMenu to Kotlin

--- src/main/java/gruvexp/bbminigames/menu/PaginatedMenu.kt
+++ src/main/java/gruvexp/bbminigames/menu/PaginatedMenu.kt
@@ -1,55 +1,55 @@
-package gruvexp.bbminigames.menu;
+package gruvexp.bbminigames.menu
 
-import net.kyori.adventure.text.Component;
-import org.bukkit.NamespacedKey;
-import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.persistence.PersistentDataType;
+import net.kyori.adventure.text.Component
+import org.bukkit.NamespacedKey
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 
-public abstract class PaginatedMenu extends Menu {
-
-    public enum PageAction {
+abstract class PaginatedMenu : Menu() {
+    enum class PageAction {
         PREV,
         NEXT
     }
-    public static final NamespacedKey KEY_PAGE_ACTION = new NamespacedKey("botbows", "page_action");
 
-    public static final ItemStack PAGE_PREV = makeItem("prev", Component.text("Prev"), KEY_PAGE_ACTION, PageAction.PREV.name());
-    public static final ItemStack PAGE_NEXT = makeItem("next", Component.text("Next"), KEY_PAGE_ACTION, PageAction.NEXT.name());
-
-    protected void setPageButtons(int rowIndex, boolean prevMenuButton, boolean nextMenuButton) {
-        inventory.setItem(rowIndex*9    , VOID);
-        inventory.setItem(rowIndex*9 + 1, VOID);
-        inventory.setItem(rowIndex*9 + 2, VOID);
-        inventory.setItem(rowIndex*9 + 3, prevMenuButton ? PAGE_PREV : VOID);
-        inventory.setItem(rowIndex*9 + 4, VOID);
-        inventory.setItem(rowIndex*9 + 5, nextMenuButton ? PAGE_NEXT : VOID);
-        inventory.setItem(rowIndex*9 + 6, VOID);
-        inventory.setItem(rowIndex*9 + 7, VOID);
-        inventory.setItem(rowIndex*9 + 8, VOID);
+    protected fun setPageButtons(rowIndex: Int, prevMenuButton: Boolean, nextMenuButton: Boolean) {
+        inventory.setItem(rowIndex * 9    , VOID)
+        inventory.setItem(rowIndex * 9 + 1, VOID)
+        inventory.setItem(rowIndex * 9 + 2, VOID)
+        inventory.setItem(rowIndex * 9 + 3, if (prevMenuButton) PAGE_PREV else VOID)
+        inventory.setItem(rowIndex * 9 + 4, VOID)
+        inventory.setItem(rowIndex * 9 + 5, if (nextMenuButton) PAGE_NEXT else VOID)
+        inventory.setItem(rowIndex * 9 + 6, VOID)
+        inventory.setItem(rowIndex * 9 + 7, VOID)
+        inventory.setItem(rowIndex * 9 + 8, VOID)
     }
 
-    protected void prevPage(Player p) {}
-    protected void nextPage(Player p) {}
+    protected open fun prevPage(p: Player) {}
+    protected open fun nextPage(p: Player) {}
 
-    protected boolean handlePageClick(InventoryClickEvent e) {
-        ItemStack clickedItem = e.getCurrentItem();
-        if (clickedItem == null || !clickedItem.hasItemMeta()) return false;
+    protected fun handlePageClick(e: InventoryClickEvent): Boolean {
+        val clickedItem = e.getCurrentItem() ?: return false
+        if (!clickedItem.hasItemMeta()) return false
 
-        String actionStr = clickedItem.getItemMeta().getPersistentDataContainer()
-                .get(KEY_PAGE_ACTION, PersistentDataType.STRING);
+        val actionStr = clickedItem.itemMeta.persistentDataContainer
+            .get(KEY_PAGE_ACTION, PersistentDataType.STRING) ?: return false
 
-        if (actionStr == null) return false;
-
-        Player p = (Player) e.getWhoClicked();
-        PageAction action = PageAction.valueOf(actionStr);
+        val p = e.whoClicked as Player
+        val action = PageAction.valueOf(actionStr)
 
         if (action == PageAction.PREV) {
-            prevPage(p);
+            prevPage(p)
         } else if (action == PageAction.NEXT) {
-            nextPage(p);
+            nextPage(p)
         }
-        return true;
+        return true
+    }
+
+    companion object {
+        val KEY_PAGE_ACTION: NamespacedKey = NamespacedKey("botbows", "page_action")
+
+        val PAGE_PREV: ItemStack = makeItem("prev", Component.text("Prev"), KEY_PAGE_ACTION, PageAction.PREV.name)
+        val PAGE_NEXT: ItemStack = makeItem("next", Component.text("Next"), KEY_PAGE_ACTION, PageAction.NEXT.name)
     }
 }
