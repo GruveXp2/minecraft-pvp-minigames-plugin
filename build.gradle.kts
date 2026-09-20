852ce19be05f87b988e1d64e684b2ffbf7d03576
Gruve <gruveexp123@gmail.com>
2026-09-20 21:47:34 +0200

    bump version to 1.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ val buildNumber: Int = if (buildNumberFile.exists()) {
 }
 
 group = "gruvexp"
-version = "5.0.0-$buildNumber"
+version = "5.1.0-$buildNumber"
 description = "The plugin used on the BotBows minigames server"
 
 java {
